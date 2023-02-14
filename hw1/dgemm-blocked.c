7cb159031a41b290632ea05304d6dc95b45e0453
Chuhan <co232@cornell.edu>
2023-02-14 03:19:18 -0800

    reached 23% with SIMD

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -5,83 +5,69 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #ifndef BLOCK_SIZE
 #define BLOCK_SIZE 24
 #endif
-
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
-void micro_kernel(double* A, double* B, double* C)
-{
-    __m256d Ar;
-    __m256d Br;
-    __m256d Cr;
-    __m256d Dr;
+void foo(double s, double* a, double* c, int n);
 
-    // Load
-    Ar = _mm256_set1_pd(A[0]);
-    Br = _mm256_load_pd(B);
-    Cr = _mm256_load_pd(C);
+void foo2(double s, double *a, double *c, int n);
 
-    // Compute
-    Dr = _mm256_mul_pd(Ar, Br);
-    Dr = _mm256_add_pd(Dr, Cr);
+void foo3(double s, double *a, double *c, int n);
 
-    // Store
-    _mm256_store_pd(C, Dr);
-
-}
-/*
- * This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N.
- */
-static void do_block(int lda, int M, int N, int K, double* A, double* B, double* C) {
-    for (int k = 0; k < K; ++k)
+void square_dgemm(int n, double* A, double* B, double* C) {
+    // For each row i of A
+    for (int j = 0; j < n; ++j)
     {
-        for (int i = 0; i < M; ++i)
+        for (int k = 0; k < n; ++k)
         {
-            for (int j = 0; j < N; j+=4)
-            {
-                //printf("k: %d, i: %d, j: %d", k, i, j);
-                // A[i][k] one element, B[k][j], C[i][j]
-                //micro_kernel(A + i + k * lda, B + k + j * lda, C + i + j * lda);
-                double cij = C[i + j * lda];
-                cij += A[i + k * lda] * B[k + j * lda];
-                C[i + j * lda] = cij;
-            }
+            //foo(B[k + j * n], A + k * n, C + j * n, n);
+            int largestMul = n - (n % 8);
+            int diff = n - largestMul;
+            foo3(B[k + j * n], A + k * n, C + j * n, largestMul);
+            foo(B[k + j * n], A + k * n + largestMul, C + j * n + largestMul, diff);
         }
     }
-    // For each row i of A
-    // for (int i = 0; i < M; ++i) {
-    //     // For each column j of B
-    //     for (int j = 0; j < N; ++j) {
-    //         // Compute C(i,j)
-    //         double cij = C[i + j * lda];
-    //         for (int k = 0; k < K; ++k) {
-    //             cij += A[i + k * lda] * B[k + j * lda];
-    //         }
-    //         C[i + j * lda] = cij;
-    //     }
-    // }
 }
 
-/* This routine performs a dgemm operation
- *  C := C + A * B
- * where A, B, and C are lda-by-lda matrices stored in column-major format.
- * On exit, A and B maintain their input values. */
-void square_dgemm(int lda, double* A, double* B, double* C) {
-    printf("Here");
-    // For each block-row of A
-    for (int i = 0; i < lda; i += BLOCK_SIZE) {
-        // For each block-column of B
-        for (int j = 0; j < lda; j += BLOCK_SIZE) {
-            // Accumulate block dgemms into block of C
-            for (int k = 0; k < lda; k += BLOCK_SIZE) {
-                // Correct block dimensions if block "goes off edge of" the matrix
-                int M = min(BLOCK_SIZE, lda - i);
-                int N = min(BLOCK_SIZE, lda - j);
-                int K = min(BLOCK_SIZE, lda - k);
-                // Perform individual block dgemm
-                do_block(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
-            }
-        }
+void foo3(double s, double *A, double *C, int n)
+{
+    // element wise multiply a and s
+    // then add result back to c
+    for (int i = 0; i < n/8; i++)
+    {
+        // Declare
+        double A0, A1, A2, A3, A4, A5, A6, A7;
+        double C0, C1, C2, C3, C4, C5, C6, C7;
+
+        // Load
+        A0 = A[i * 8 + 0], A1 = A[i * 8 + 1], A2 = A[i * 8 + 2], A3 = A[i * 8 + 3], A4 = A[i * 8 + 4], A5 = A[i * 8 + 5], A6 = A[i * 8 + 6], A7 = A[i * 8 + 7];
+        C0 = C[i * 8 + 0], C1 = C[i * 8 + 1], C2 = C[i * 8 + 2], C3 = C[i * 8 + 3], C4 = C[i * 8 + 4], C5 = C[i * 8 + 5], C6 = C[i * 8 + 6], C7 = C[i * 8 + 7];
+
+        // Compute
+        C0 = s * A0 + C0, C1 = s * A1 + C1, C2 = s * A2 + C2, C3 = s * A3 + C3, C4 = s * A4 + C4, C5 = s * A5 + C5, C6 = s * A6 + C6, C7 = s * A7 + C7;
+
+        // Store
+        C[i * 8 + 0] = C0, C[i * 8 + 1] = C1, C[i * 8 + 2] = C2, C[i * 8 + 3] = C3, C[i * 8 + 4] = C4, C[i * 8 + 5] = C5, C[i * 8 + 6] = C6, C[i * 8 + 7] = C7;
+
+    }
+}
+
+void foo2(double s, double *a, double *c, int n)
+{
+    __m256d sv = _mm256_set1_pd(s);
+    for (int i = 0; i < n/4; i++)
+    {
+        __m256d cv = _mm256_loadu_pd(&c[4 * i]);
+        __m256d av = _mm256_loadu_pd(&a[4 * i]);
+        __m256d t1 = _mm256_mul_pd(sv, av);
+        cv = _mm256_add_pd(t1, cv);
+        _mm256_storeu_pd(&c[4 * i], cv);
+    }
+}
+
+void foo(double s, double* a, double* c, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        c[i] = s * a[i] + c[i];
     }
 }
